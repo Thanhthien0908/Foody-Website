c1a9415c9ab5cb211a10f4c884a82b05ada7a045
Nguyen Thanh Thien <thiennt@i.hivetech.vn>
2021-03-17 12:44:29 +0700

    update-admin

--- admin/src/component/AccUser/index.js
+++ admin/src/component/AccUser/index.js
@@ -1,42 +1,73 @@
 import React,{useState} from 'react';
 import './styles.scss';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
+
 const AccUser = () => {
     const [openForm, setopenForm] = useState(false);
+    const [dataAccUser, setDataAccUser] = useState([
+        {
+            username: "thiennt",
+            pass: "1234",
+            sdt: "0345000921",
+            address: "HN"
+        },
+        {
+            username: "thienNguyen",
+            pass: "123456",
+            sdt: "0345000921",
+            address: "HN"
+        }
+    ]);
+    const [objItems, setobjItems] = useState({
+        username: '',
+        pass: '',
+        sdt: '',
+        address: ''
+    });
     const showForm = () =>{
         setopenForm(!openForm);
-        console.log(openForm);
     }
+    const onChange = (e) =>{
+        let target = e.target;
+        let name = target.name;
+        let value = target.value;
+        setobjItems({...objItems,[name]: value});
+    }
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        setDataAccUser([...dataAccUser,objItems]);
+        // console.log(dataAccUser);
+        // localStorage.setItem("accAdmin",JSON.stringify(dataAccUser));
+        setobjItems({
+            username: '',
+            pass: '',
+            sdt: '',
+            address: ''
+        })
+    }
+    const onDelete = (index) =>{
+        setDataAccUser(dataAccUser.splice(0,index).concat(dataAccUser.splice(index+1)));
+        console.log(dataAccUser);
+    } 
+    
     return (
         <div className="table-responsive">
-            <h1>Quản lý tài khoản Users</h1>
+            <h1>Quản lý tài khoản Client</h1>
             <button className="btn btn-success" onClick={showForm}>Thêm tài khoản</button>
-            <Form className={openForm ? "openForm" : "hideForm"}>
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Tên Tài Khoản</Form.Label>
-                    <Form.Control placeholder="Enter Acc" />
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label>Mật Khẩu</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                </Form.Row>
-                <Form.Group controlId="formGridPhone">
-                    <Form.Label>Số điện thoại</Form.Label>
-                    <Form.Control placeholder="Phone Number" />
-                </Form.Group>
-                <Form.Group controlId="formGridAddress2">
-                    <Form.Label>Địa chỉ</Form.Label>
-                    <Form.Control placeholder="Address" />
-                </Form.Group>
-                <Button variant="danger" type="submit">
-                    Lưu
-                </Button>
-                </Form>
-                <table className="table product-table">
+            {/* <Form className={openForm ? "openForm" : "hideForm"}> */}
+               
+            <form onSubmit={onSubmit} className={openForm ? "openForm" : "hideForm"}>
+                <label className="label-input" for="fname">UserName : </label><br />
+                <input className="inputForm" type="text" name="username" value={objItems.username} onChange={onChange}/><br />
+                <label className="label-input" for="fname">Password : </label><br />
+                <input className="inputForm" type="text" name="pass" value={objItems.pass} onChange={onChange}/><br />
+                <label className="label-input" for="fname">Số điện thoại : </label><br />
+                <input className="inputForm" type="text" name="sdt" value={objItems.sdt} onChange={onChange}/><br />
+                <label className="label-input" for="fname">Địa Chỉ : </label><br />
+                <input className="inputForm" type="text" name="address" value={objItems.address} onChange={onChange}/><br />   
+                <button type="submit" className="btn-save btn btn-danger">Lưu</button>
+            </form>
+
+            <table className="table product-table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
@@ -48,22 +79,18 @@ const AccUser = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <th>thiennt</th>
-                    <th>mkmkmkm</th>
-                    <th>0345000921</th>
-                    <th>Kiều mai - Bắc Từ Liêm - Hà Nội</th>
-                    <th><button className="btn btn-primary">Sửa</button><button className="btn btn-danger">Xóa</button></th>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <th>thiennt</th>
-                    <th>mkmkmkm</th>
-                    <th>0345000921</th>
-                    <th>Kiều mai - Bắc Từ Liêm - Hà Nội</th>
-                    <th><button className="btn btn-primary">Sửa</button><button className="btn btn-danger">Xóa</button></th>
-                </tr>
+                {dataAccUser.map((item,index) => {
+                    return (
+                        <tr>
+                        <th scope="row">{index+1}</th>
+                        <th>{item.username}</th>
+                        <th>{item.pass}</th>
+                        <th>{item.sdt}</th>
+                        <th>{item.address}</th>
+                        <th><button className="btn btn-danger" onClick={()=>onDelete(index)}>Xóa</button></th>
+                        </tr>
+                    );
+                })}
             </tbody>
             </table>
         </div>
--- admin/src/component/DanhMucDoUong/index.js
+++ admin/src/component/DanhMucDoUong/index.js
@@ -1,41 +1,73 @@
 import React,{useState} from 'react';
 import './styles.scss';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
+
 const DanhMucDoUong = () => {
     const [openForm, setopenForm] = useState(false);
+    const [dataDouong, setDataDoUong] = useState([
+        {
+            foodname: "Pepsi",
+            img: "mkmkmkm.jpg",
+            price: "86.000đ",
+            type: "Đồ uống"
+        },
+        {
+            foodname: "Pepsi",
+            img: "mkmkmkm.jpg",
+            price: "86.000đ",
+            type: "Đồ uống"
+        }
+        
+    ]);
+    const [objItems, setobjItems] = useState({
+        foodname: '',
+        img: '',
+        price: '',
+        type: ''
+    });
     const showForm = () =>{
         setopenForm(!openForm);
-        console.log(openForm);
     }
+    const onChange = (e) =>{
+        let target = e.target;
+        let name = target.name;
+        let value = target.value;
+        setobjItems({...objItems,[name]: value});
+    }
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        setDataDoUong([...dataDouong,objItems]);
+        // console.log(dataAccAdmin);
+        // localStorage.setItem("accAdmin",JSON.stringify(dataAccAdmin));
+        setobjItems({
+            foodname: '',
+            img: '',
+            price: '',
+            type: ''
+        })
+    }
+    const onDelete = (index) =>{
+        setDataDoUong(dataDouong.splice(0,index).concat(dataDouong.splice(index+1)));
+        console.log(dataDouong);
+    } 
+    
     return (
         <div className="table-responsive">
-            <h1>Quản lý danh mục đồ uống</h1>
-            <button className="btn btn-success" onClick={showForm}>Thêm đồ uống</button>
-            <Form className={openForm ? "openForm" : "hideForm"}>
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Tên đồ uống</Form.Label>
-                    <Form.Control placeholder="Enter Name" />
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label>Hình ảnh</Form.Label>
-                    <Form.Control placeholder="img" />
-                    </Form.Group>
-                </Form.Row>
-                <Form.Group controlId="formGridPhone">
-                    <Form.Label>Giá</Form.Label>
-                    <Form.Control placeholder="Price" />
-                </Form.Group>
-                <Form.Group controlId="formGridAddress2">
-                    <Form.Label>Loại</Form.Label>
-                    <Form.Control placeholder="Type" />
-                </Form.Group>
-                <Button variant="danger" type="submit">
-                    Lưu
-                </Button>
-                </Form>
+            <h1>Quản lý danh mục Đồ uống</h1>
+            <button className="btn btn-success" onClick={showForm}>Thêm Đồ uống</button>
+            {/* <Form className={openForm ? "openForm" : "hideForm"}> */}
+               
+            <form onSubmit={onSubmit} className={openForm ? "openForm" : "hideForm"}>
+                <label className="label-input" for="fname">DrinkName : </label><br />
+                <input className="inputForm" type="text" name="foodname" value={objItems.username} onChange={onChange}/><br />
+                <label className="label-input" for="fname">Ảnh : </label><br />
+                <input className="inputForm" type="text" name="img" value={objItems.pass} onChange={onChange}/><br />
+                <label className="label-input" for="fname">Giá : </label><br />
+                <input className="inputForm" type="text" name="price" value={objItems.sdt} onChange={onChange}/><br />
+                <label className="label-input" for="fname">Loại Sản phẩm : </label><br />
+                <input className="inputForm" type="text" name="type" value={objItems.address} onChange={onChange}/><br />   
+                <button type="submit" className="btn-save btn btn-danger">Lưu</button>
+            </form>
+
             <table className="table product-table">
             <thead>
                 <tr>
@@ -48,22 +80,18 @@ const DanhMucDoUong = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <th>Pepsi</th>
-                    <th>mkmkmkm.jpg</th>
-                    <th>20.000đ</th>
-                    <th>Đồ uống</th>
-                    <th><button className="btn btn-primary">Sửa</button><button className="btn btn-danger">Xóa</button></th>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <th>Pepsi</th>
-                    <th>mkmkmkm.jpg</th>
-                    <th>20.000đ</th>
-                    <th>Đồ uống</th>
-                    <th><button className="btn btn-primary">Sửa</button><button className="btn btn-danger">Xóa</button></th>
-                </tr>
+                {dataDouong.map((item,index) => {
+                    return (
+                        <tr>
+                        <th scope="row">{index+1}</th>
+                        <th>{item.foodname}</th>
+                        <th>{item.img}</th>
+                        <th>{item.price}</th>
+                        <th>{item.type}</th>
+                        <th><button className="btn btn-danger" onClick={()=>onDelete(index)}>Xóa</button></th>
+                        </tr>
+                    );
+                })}
             </tbody>
             </table>
         </div>
