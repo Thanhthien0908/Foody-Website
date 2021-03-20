bab781414d3be36519ab362b03b0e60409cf3e39
Nguyen Thanh Thien <thanhthien09081999@gmail.com>
2021-03-20 19:53:03 +0700

    login-face

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,20 +1,29 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.scss'
 import iconUser from './assets/icon-user.svg'
 import iconUnlock from './assets/icon-unlock.svg'
 import AOS from "aos";
 import "aos/dist/aos.css";
-import {Modal} from 'antd';
-import {Redirect, useHistory} from 'react-router-dom';
+import { Modal } from 'antd';
+import { Redirect, useHistory } from 'react-router-dom';
+import FacebookLogin from 'react-facebook-login';
 const Login = () => {
     let history = useHistory();
+    const [logInFace,setLogInFace] = useState("");
+    const responseFacebook = (response) => {
+        setLogInFace(response.accessToken)
+    }
+    console.log("access",logInFace);
     const onLogin = () => {
         const userName = document.getElementById('userName').value;
         const passWord = document.getElementById('passWord').value;
-        if(userName === "thiennt" && passWord === "123456"){
+        if (userName === "thiennt" && passWord === "123456") {
             localStorage.setItem("accessToken", true);
-            history.replace("/"); 
-        }else{
+            history.replace("/");
+        }else if(logInFace != " "){
+            localStorage.setItem("accessToken", true);
+            history.replace("/");
+        }else {
             Modal.error({
                 title: 'Đăng nhập thất bại !',
                 content: 'Tên tài khoản hoặc mật khẩu không đúng ? ',
@@ -26,27 +35,27 @@ const Login = () => {
     }
     useEffect(() => {
         AOS.init({
-        offset: 200,
-        delay: 0,
-        duration: 1000,
+            offset: 200,
+            delay: 0,
+            duration: 1000,
         });
-        }, []);
+    }, []);
     return (
         <div className="screen-login">
             <div className="screen-login__title">Foody Login</div>
             <div className="screen-login__user" data-aos="fade-right">
                 <table className="screen-login__user__group">
                     <tr>
-                        <td><input className="screen-login__user__input" type="text" id="userName" placeholder="Tên đăng nhập"/></td>
-                        <td><img className="screen-login__user__icon" src={iconUser} alt="icon_user"/></td>
+                        <td><input className="screen-login__user__input" type="text" id="userName" placeholder="Tên đăng nhập" /></td>
+                        <td><img className="screen-login__user__icon" src={iconUser} alt="icon_user" /></td>
                     </tr>
                 </table>
             </div>
             <div className="screen-login__lock" data-aos="fade-left">
                 <table className="screen-login__lock__group">
                     <tr>
-                        <td><input className="screen-login__lock__input" type="password" id="passWord" placeholder="Mật Khẩu"/></td>
-                        <td><img className="screen-login__lock__icon" src={iconUnlock} alt="icon_password"/></td>
+                        <td><input className="screen-login__lock__input" type="password" id="passWord" placeholder="Mật Khẩu" /></td>
+                        <td><img className="screen-login__lock__icon" src={iconUnlock} alt="icon_password" /></td>
                     </tr>
                 </table>
             </div>
@@ -54,7 +63,14 @@ const Login = () => {
                 <table className="screen-login__button__group">
                     <tr>
                         <td><button className="screen-login__button__login" data-aos="fade-right" onClick={onLogin}>Login</button></td>
-                        <td><button className="screen-login__button__register" data-aos="fade-left" onClick={moveRegister}>Register</button></td>
+                        {/* <td><button className="screen-login__button__register" data-aos="fade-left" onClick={moveRegister}>Register</button></td> */}
+                    </tr>
+                    <tr>
+                        <FacebookLogin
+                            appId="127955015818933"
+                            autoLoad={true}
+                            fields="name,email,picture"
+                            callback={responseFacebook} />
                     </tr>
                 </table>
             </div>
